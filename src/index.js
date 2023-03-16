c4c7477cceb839b57ece3413a6a48d90acb060d5
Avichez <starkavichi@gmail.com>
2023-03-16 09:29:57 +0200

    changed port and added engine version

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 const { createServer } = require('./createServer');
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 createServer().listen(port, () => {
   console.log(`Server is running on port: ${port}`);
