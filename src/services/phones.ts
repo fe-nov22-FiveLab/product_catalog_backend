6af800c6e0b7982561751b77d4eedb4c0da476d6
Avichez <starkavichi@gmail.com>
2023-03-21 13:10:48 +0200

    changed request to DB

--- src/services/phones.ts
+++ src/services/phones.ts
@@ -1,3 +1,4 @@
+import { PhoneDetail } from './../models/PhonesDetails';
 import { Phone } from '../models/Phone';
 
 export const getAll = async (sort?: string) => {
@@ -9,7 +10,13 @@ export const getAll = async (sort?: string) => {
   }
 
   const phones = await Phone.findAll({
-    order: [[sortBy, sortType]]
+    order: [[sortBy, sortType]],
+    include: [
+      {
+        model: PhoneDetail,
+        as: 'phoneDetails'
+      }
+    ]
   });
 
   const total = await Phone.count();
@@ -24,7 +31,13 @@ export const getOne = (phoneId: string) =>
   Phone.findOne({
     where: {
       phoneId
-    }
+    },
+    include: [
+      {
+        model: PhoneDetail,
+        as: 'phoneDetails'
+      }
+    ]
   });
 
 export const getByParts = async (
@@ -43,7 +56,13 @@ export const getByParts = async (
   const phones = await Phone.findAll({
     offset,
     limit: perPage,
-    order: [[sortBy, sortType]]
+    order: [[sortBy, sortType]],
+    include: [
+      {
+        model: PhoneDetail,
+        as: 'phoneDetails'
+      }
+    ]
   });
 
   const total = await Phone.count();
